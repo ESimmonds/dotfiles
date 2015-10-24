59146ef948b393d7813086a10df888be5980bd7d
Equan Simmonds <esimmonds@ncarb.org>
2015-10-24 00:08:09 -0400

    trim some fat

--- brew-cask.sh
+++ brew-cask.sh
@@ -12,37 +12,33 @@ brew tap caskroom/versions
 
 # daily
 brew cask install spectacle
-brew cask install dropbox
+brew cask install evernote
 brew cask install gyazo
-brew cask install onepassword
 brew cask install rescuetime
 brew cask install flux
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install imagealpha
+brew install Caskroom/cask/iterm2
+brew cask install sublime-text3
 brew cask install imageoptim
 
 # fun
-brew cask install limechat
-brew cask install miro-video-converter
-brew cask install horndis               # usb tethering
+#brew cask install miro-video-converter
+#brew cask install horndis               # usb tethering
 
 # browsers
 brew cask install google-chrome-canary
-brew cask install firefox-nightly
-brew cask install webkit-nightly
-brew cask install chromium
+#brew cask install firefox-nightly
+#brew cask install webkit-nightly
+#brew cask install chromium
 brew cask install torbrowser
 
 # less often
 brew cask install disk-inventory-x
-brew cask install screenflow4 # 4 specifically not 5.
 brew cask install vlc
-brew cask install gpgtools
 brew cask install licecap
-brew cask install utorrent
+#brew cask install gpgtools
+
 
 
 # Not on cask but I want regardless.
